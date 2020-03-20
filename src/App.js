b0cfe9bb2df2b128415c003e20556091c4859251
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2020-03-20 14:13:57 -0600

    Adding the search component rendering with different endpoint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
 import Navbar from "./components/Layout/NavBar";
 import Users from "./components/Users/Users";
+import Search from "./components/Users/search";
 import axios from "axios";
 
 import "./App.css";
 
 class App extends Component {
   state = {
-    users:[],
-    loading:false
-  }
-  async componentDidMount() {
-    this.setState({loading:true});
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-    this.setState({users:res.data,loading:false})
-  }
+    users: [],
+    loading: false
+  };
+  searchUsers = async text => {
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({users: res.data.items, loading:false})
+  };
   render() {
     return (
       <div className="App">
         <Navbar icon="fas fa-cat" />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search searchUsers={this.searchUsers} />{" "}
+          <Users loading={this.state.loading} users={this.state.users} />{" "}
         </div>{" "}
       </div>
     );
